95444d5075ddceb47af6d441b6e5fd5eb80e3da2
inDemniFi Crypto <118511874+inDemniFi@users.noreply.github.com>
2023-08-19 17:32:29 -0500

    Update WalletProvider.js

--- src/WalletProvider.js
+++ src/WalletProvider.js
@@ -13,7 +13,7 @@ function WalletProvider() {
         <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;400;600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link href="style.css" rel="stylesheet" type="text/css" />
+        <link href="style.css" rel="stylesheet" type="text/css">
       </head>
       
       <body>
